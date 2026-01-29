98480fd17b9cecc415e4c875cdea90b3e309d3bd
onuralp-t <149798217+onuralp-t@users.noreply.github.com>
2026-01-29 14:19:17 +0300

    made bingen CLI somewhat more functional

--- bingen.c
+++ bingen.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <string.h>
 #include <time.h>
 
 #define COMMON_IMPLEMENTATION
@@ -18,25 +19,51 @@ void rand_i64_write(const char* dst_path, int64_t buffer_count, xo_state* rng_st
     fclose(f);
 }
 
+char** next_arg(int *argc, char ***argv)
+{
+    if (*argc > 1) {*argc -= 1; return *argv += 1;}
+    else {return 0;}
+}
+
 int main(int argc, char* argv[])
 {
     xo_state rng = {0};
     srand_xo(&rng, time(0));
 
-    const char* output_file   = "out.bin";
-    const char* usage_message = "USAGE: ./bingen <number>\n";
+    const char* default_output = "out.bin";
+    const char* usage_message  = "USAGE: ./bingen <number> [-o output_path]\n";
 
-    if (argc == 1) {printf("%s", usage_message); return 0;}
-    else if (argc == 2)
-    {
-        char* arg = argv[2-1];
+    uint64_t PRN_count = 0;
+
+    if (next_arg(&argc, &argv)) {
+        char* arg = *argv;
         while (*arg) {
             if (!isdigit(*arg)) {printf("%s", usage_message); return 0;};
             ++arg;
-        }
+        } // is number
+        PRN_count = atoi(*argv);
 
-        rand_i64_write(output_file, atoi(argv[2-1]), &rng);
-        printf("INFO: wrote `%d` randomly generated 64-bit numbers to `%s`\n", atoi(argv[2-1]), output_file);
+        if (next_arg(&argc, &argv))
+        {
+            if (strcmp(*argv, "-o") == 0)
+            {
+                if (next_arg(&argc, &argv)) {
+                    char* arg = *argv;
+                    while (*arg) {
+                        if (!isalnum(*arg)) {printf("%s", usage_message); return 0;};
+                        ++arg;
+                    } // is alnum
+                    rand_i64_write(*argv, PRN_count, &rng);
+                    printf("INFO: wrote `%llu` randomly generated 64-bit numbers to `%s`\n", PRN_count, *argv);
+                }
+                else {printf("%s", usage_message);}
+            }
+        }
+        else
+        {
+            rand_i64_write(default_output, PRN_count, &rng);
+            printf("INFO: wrote `%llu` randomly generated 64-bit numbers to `%s`\n", PRN_count, default_output);
+        }
     }
     else {printf("%s", usage_message);}
 

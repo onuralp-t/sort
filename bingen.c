15b80d3b799385e171ae904a8d600ac9406e63f3
onuralp-t <149798217+onuralp-t@users.noreply.github.com>
2026-01-29 13:56:04 +0300

    factor out output messages from bingen

--- bingen.c
+++ bingen.c
@@ -23,19 +23,22 @@ int main(int argc, char* argv[])
     xo_state rng = {0};
     srand_xo(&rng, time(0));
 
-    if (argc == 1) {printf("USAGE: ./bingen <number>\n");}
+    const char* output_file   = "out.bin";
+    const char* usage_message = "USAGE: ./bingen <number>\n";
+
+    if (argc == 1) {printf("%s", usage_message); return 0;}
     else if (argc == 2)
     {
         char* arg = argv[2-1];
         while (*arg) {
-            if (!isdigit(*arg)) {printf("USAGE: ./bingen <number>\n"); return 0;};
+            if (!isdigit(*arg)) {printf("%s", usage_message); return 0;};
             ++arg;
         }
 
-        rand_i64_write("out.bin", atoi(argv[2-1]), &rng);
-        printf("INFO: wrote `%d` randomly generated 64-bit numbers to `out.bin`\n", atoi(argv[2-1]));
+        rand_i64_write(output_file, atoi(argv[2-1]), &rng);
+        printf("INFO: wrote `%d` randomly generated 64-bit numbers to `%s`\n", atoi(argv[2-1]), output_file);
     }
-    else {printf("USAGE: ./bingen <number>\n");}
+    else {printf("%s", usage_message);}
 
     return 0;
 }
